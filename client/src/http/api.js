038970ca624af2045b41b848cb6feb88577d349c
ashith-vs <ashith855@gmail.com>
2025-07-05 19:50:27 +0530

    refactor:  update MongoDB connection string to mongodb atlas

--- client/src/http/api.js
+++ client/src/http/api.js
@@ -47,11 +47,11 @@ const fetchData = async (endpoint, method = 'GET', data = {}, headers = {}) => {
             // Server responded with a status other than 2xx
             throw new Error(error.response.data.message || 'An error occurred');
         } else if (error.request) {
-            console.log("error.request", error.request);
+            // console.log("error.request", error.request);
             // Request was made but no response received
             throw new Error('No response received from server');
         } else {
-            console.log("error.message", error.message)
+            // console.log("error.message", error.message)
             // Something happened in setting up the request
             throw new Error(error.message);
         }
